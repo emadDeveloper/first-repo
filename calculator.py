681dd74f078f747f41e923b027915bf65d3c00e6
emadDeveloper <mde63328@gmail.com>
2023-11-11 02:08:20 -0800

    first commit

---
+++ calculator.py
@@ -0,0 +1,5 @@
+print('this is my first repository')
+print('this is my first repository')
+print('this is my first repository')
+print('this is my first repository')
+print('this is my first repository')
